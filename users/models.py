06f05bc56b32664f2b0031f5fb676a0572d60a33
claus42 <sometaclaus@gmail.com>
2015-02-28 16:15:06 +0200

    Added moder for storing users data.

--- users/models.py
+++ users/models.py
@@ -1,11 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+class SynergyUser(User):
     phone = models.CharField(max_length=20)
     
-    def __str__(self):
-        return self.user_name
